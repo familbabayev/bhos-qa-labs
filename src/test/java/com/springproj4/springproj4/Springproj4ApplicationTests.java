06999a870109cf425bdfa848576c9f6b9626600a
familbabayev <familhacker@gmail.com>
2021-10-30 18:49:00 +0400

    feature/lab8

---
+++ src/test/java/com/springproj4/springproj4/Springproj4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springproj4.springproj4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springproj4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
