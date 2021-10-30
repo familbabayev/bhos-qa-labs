bcd45092283d8c1b554a67e5cab00186ee0cdd3e
familbabayev <familhacker@gmail.com>
2021-10-30 09:55:17 +0400

    feature/lab7

---
+++ src/main/java/com/springproj3/springproj3/FirebaseInitialization.java
@@ -0,0 +1,28 @@
+package com.springproj3.springproj3;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Service
+public class FirebaseInitialization {
+
+    @PostConstruct
+    public void initializer() throws IOException {
+
+        FileInputStream serviceAccount = new FileInputStream("./firebasekey.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+    }
+
+}
