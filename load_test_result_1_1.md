36508a899f3ab92d84a69bbbe81a4d646ebd38aa
familbabayev <familhacker@gmail.com>
2021-12-02 22:31:59 +0400

    feature/lab13

---
+++ load_test_result_1_1.md
@@ -0,0 +1,11 @@
+# Load test 1
+
+![](load_test_result_1_1.PNG)
+
+## Test environment specifications
+##### OS Name
+Windows 10 Pro (x64)
+##### Processor
+Intel(R) Core(TM) i5-4300U CPU @ 1.90GHz, 2501 MHz, 2 Core(s), 4 Logical Processors(s)
+##### RAM
+8.0 GB
--- src/main/java/com/springprog8/springprog8/Service.java
+++ src/main/java/com/springprog8/springprog8/Service.java
@@ -1,11 +1,12 @@
 package com.springprog8.springprog8;
 
-import com.nimbusds.srp6.SRP6CryptoParams;
-import com.nimbusds.srp6.SRP6Exception;
-import com.nimbusds.srp6.SRP6ServerSession;
+import com.nimbusds.srp6.*;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigInteger;
 
@@ -42,4 +43,41 @@ public class Service {
         }
 
     }
+
+    RestTemplate restTemplate = new RestTemplate();
+    HttpHeaders headers = new HttpHeaders();
+
+    @GetMapping("/srpLoadTest")
+    public ResponseEntity<Object> srpLoadTest() throws SRP6Exception {
+        // New client session
+        SRP6ClientSession Client = new SRP6ClientSession();
+        String userID = "famil";
+        String password = ";;01?property?DESTROY?build?96;;";
+        Client.step1(userID, password);
+
+        HttpEntity<String> newSesEntity = new HttpEntity<>(userID, headers);
+        String newSesResponse = restTemplate.exchange("http://localhost:8080/new_session",
+                HttpMethod.POST, newSesEntity, String.class).getBody();
+
+        BigInteger serverPublicB = new BigInteger(newSesResponse);
+
+        // Compute values
+        SRP6CryptoParams defaultCryptoParams = SRP6CryptoParams.getInstance();
+        BigInteger saltS = new BigInteger(salt);
+        SRP6ClientCredentials clientPublicAEvidenceM1 = Client.step2(defaultCryptoParams, saltS, serverPublicB);
+
+        CompValReqBody compValBody = new CompValReqBody(clientPublicAEvidenceM1.A, clientPublicAEvidenceM1.M1);
+        HttpEntity<CompValReqBody> compValEntity = new HttpEntity<>(compValBody, headers);
+
+        String serverEvidenceM2 = restTemplate.postForEntity("http://localhost:8080/compute_values",
+                compValEntity, String.class).getBody();
+
+
+        if (serverEvidenceM2 == "") {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+        } else {
+            return ResponseEntity.status(HttpStatus.OK).body(serverEvidenceM2);
+        }
+    }
+
 }
