0d32b0011e2c0fd8ccf9cbe384b53faf52797000
familbabayev <familhacker@gmail.com>
2021-12-10 19:04:59 +0400

    feature/lab14

---
+++ load_test_result_2_3.md
@@ -0,0 +1,13 @@
+# Load test 1
+
+## Test environment specifications
+##### OS Name
+Windows 10 Pro (x64)
+##### Processor
+Intel(R) Core(TM) i5-4300U CPU @ 1.90GHz, 2501 MHz, 2 Core(s), 4 Logical Processors(s)
+##### RAM
+8.0 GB
+
+![image](LoadTest100000Global.png)
+
+![image](LoadTest100000Details.png)
---
+++ src/main/java/com/springproj9/springproj9/Springproj9Application.java
@@ -0,0 +1,13 @@
+package com.springproj9.springproj9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springproj9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springproj9Application.class, args);
+	}
+
+}
