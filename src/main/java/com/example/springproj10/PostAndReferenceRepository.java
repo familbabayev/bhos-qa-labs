5d82a4901fdf591d2ad25b8131690228cf9691f4
familbabayev <familhacker@gmail.com>
2021-12-18 02:00:48 +0400

    lab/feature15

---
+++ src/main/java/com/example/springproj10/PostAndReferenceRepository.java
@@ -0,0 +1,6 @@
+package com.example.springproj10;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostAndReferenceRepository extends JpaRepository<PostAndReference, Long> {}
+
